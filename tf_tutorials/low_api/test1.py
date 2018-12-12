3c8bc312981d5a07c08c6d1317cce15fb95c316d
silvershine157 <silvershine157@gmail.com>
2018-12-13 03:55:07 +0900

    train linear model

--- tf_tutorials/low_api/test1.py
+++ tf_tutorials/low_api/test1.py
@@ -92,5 +92,41 @@ def main5():
     # execute layers
     print(sess.run(y, {x: [[1, 2, 3], [4, 5, 6]]}))
 
+# let me skip the feature column example
+
+def main6():
+    # define data
+    x = tf.constant([[1], [2], [3], [4]], dtype=tf.float32)
+    y_true = tf.constant([[0], [-1], [-2], [-3]], dtype=tf.float32)
     
-main5()
+    # define model
+    linear_model = tf.layers.Dense(units=1)
+    y_pred = linear_model(x)
+
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    
+    print("Prediction before training:")
+    print(sess.run(y_pred))
+
+    # define loss
+    loss = tf.losses.mean_squared_error(labels=y_true, predictions=y_pred)
+    print("Loss before training:")
+    print(sess.run(loss))
+
+    # training
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
+    train = optimizer.minimize(loss)
+    # 'train' is an op, not a tensor (no value produced)
+    # so evaluate 'loss' each time
+    print("Start training")
+    for i in range(100):
+        _, loss_value = sess.run((train, loss))
+        print(loss_value)
+    
+    print("Prediction after training")
+    print(sess.run(y_pred))
+    
+    
+main6()
