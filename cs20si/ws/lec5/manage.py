0f51e2d08b899fcc8f0f6eb8bd341ef3b693c97d
Eunhyouk Shin <silvershine157@gmail.com>
2019-01-11 20:53:35 +0900

    save restore, do not intialize after restoring!

--- cs20si/ws/lec5/manage.py
+++ cs20si/ws/lec5/manage.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import os
 
 # simple example for good management practice
 
@@ -7,11 +8,11 @@ import numpy as np
 def generate_dataset():
     # 3D feature, 1D label
     TRUE_W = np.array([2.0, -1.0, 3.0])
-    TRUE_B = [-3.0]
+    TRUE_B = -3.0
     ds_size = 10000
     X = np.random.normal(0.0, 3.0, [ds_size, 3])
     noise = np.random.normal(0.0, 0.5, [ds_size])
-    Y = np.einsum('nm,m->n', X, (TRUE_W))
+    Y = np.einsum('nm,m->n', X, (TRUE_W)) + TRUE_B
     Y = Y + noise
     X = X.astype(np.float32)
     Y = Y.astype(np.float32)
@@ -22,7 +23,7 @@ def generate_dataset():
 
 def main():
 
-    num_epochs = 100
+    num_epochs = 50
     batch_size = 128
    
     ds = generate_dataset()
@@ -41,12 +42,23 @@ def main():
         w = tf.get_variable('weight', [3], initializer=tf.random_normal_initializer())
         b = tf.get_variable('bias', [1], initializer=tf.constant_initializer(0.0))
         Y_pred = tf.einsum('nm,m->n', X, w) + b
+    global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
     loss = tf.reduce_sum(tf.square(Y - Y_pred))
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss, global_step=global_step)
+
+    saver = tf.train.Saver()
 
     # train
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+        ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/checkpoint'))
+        if ckpt and ckpt.model_checkpoint_path:
+            print("restoring saved model")
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print("no checkpoint found")
+
+        writer = tf.summary.FileWriter('graphs/manage', sess.graph)        
         for i in range(num_epochs):
             sess.run(train_init)
             total_loss = 0
@@ -58,5 +70,11 @@ def main():
                 pass
             print("Training epoch {0} total loss {1}".format(i, total_loss))
 
+            if(i + 1)%10 == 0:
+                saver.save(sess, 'checkpoints/manage', i)
+
+        w_learn, b_learn = sess.run([w, b])
+        print("Learnt parameters: W:{0}, b:{1}".format(w_learn, b_learn))
+        writer.close()
 
 main()
