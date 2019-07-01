14888284e073a5d342d2e3734e00beade95ad2ae
Eunhyouk Shin <silvershine157@gmail.com>
2019-07-01 17:12:46 +0900

    encoder and attention module

--- pytorch/tutorials/chatbot/main.py
+++ pytorch/tutorials/chatbot/main.py
@@ -286,3 +286,61 @@ print("mask:", mask)
 print("max_target_len", max_target_len)
 '''
 
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+
+        # input dim is also hidden_size
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
+                            dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        embedded = self.embedding(input_seq)
+        packed = nn.utils.rnn.pack_padded_sequences(embedded, input_lengths)
+        outputs, hidden = self.gru(packed, hidden) # outputs and final hidden state
+        outputs, _ = nn.utils.rnn.pad_packed_sequences(outputs)
+        # sum outputs for 2 directions
+        outputs = outputs[:, :, :self.hidden_size] + outputs[:, :, self.hidden_size:]
+        return outputs, hidden
+
+# Luong attention layer
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']
+            raise ValueError(self.method, "is not an appropriate attention method.")
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+        def dot_score(self, hidden, encoder_output):
+            return torch.sum(hidden * encoder_output, dim=2)
+
+        def general_score(self, hidden, encoder_output):
+            energy = self.attn(encoder_output)
+            return torch.sum(hidden * energy, dim=2)
+
+        def concat_score(self, hidden, encoder_output):
+            energy = self.attn(
+                torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)
+            ).tanh()
+            return torch.sum(self.v * energy, dim=2)
+
+        def forward(self, hidden, encoder_outputs):
+            if self.method == 'general':
+                attn_energies = self.general_score(hidden, encoder_outputs)
+            elif self.method == 'concat':
+                attn_energies = self.concat_score(hidden, encoder_outputs)
+            elif self.method == 'dot':
+                attn_energies = self.dot_score(hidden, encoder_outputs)
+
+            attn_energies = attn_energies.t()
+            return F.softmax(attn_energies, dim=1).unsqueeze(1)
+            
