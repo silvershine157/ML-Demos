4a8f11aa462f074cbc003bec62d5ecc555458469
Eunhyouk Shin <silvershine157@gmail.com>
2019-07-02 17:37:40 +0900

    training loop

--- pytorch/tutorials/chatbot/main.py
+++ pytorch/tutorials/chatbot/main.py
@@ -554,3 +554,49 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
     return sum(print_losses) / n_totals
 
 
+def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer, embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size, print_every, save_every, clip, corpus_name, loadFilename):
+        
+        # Load batches for each iteration
+        training_batches = [batch2TrainData(voc, [random.choice(pairs) for _ in range(batch_size)])
+                                for _ in range(n_iteration)]
+
+        # Initializations
+        print('Initializing ...')
+        start_iteration = 1
+        print_loss = 0
+        if loadFilename:
+            start_iteration = checkpoint['iteration'] + 1
+
+        # Training loop
+        print('Training...')
+        for iteration in range(start_iteration, n_iteration + 1):
+            training_batch = training_batches[iteration - 1]
+            # extract fields from batch
+            input_variable, lengths, target_variable, mask, max_target_len = training_batch
+
+            # run training iteration
+            loss = train(input_variable, lengths, target_variable, mask, max_target_len, encoder, decoder, embedding, encoder_optimizer, decoder_optimizer, batch_size, clip)
+            print_loss += loss
+
+            # print progress
+            if iteration % print_every == 0:
+                print_loss_avg = print_loss / print_every
+                print("Iteration: {}; Percent complete: {:.1f}%; Average loss: {:.4f}".format(iteration, iteration/n_iteration * 100, print_loss_avg,))
+                print_loss = 0
+
+            # save checkpoint
+            if iteration % save_every == 0:
+                directory = os.path.join(save_dir, model_name, corpus_name, '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size))
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                torch.save({
+                    'iteration': iteration,
+                    'en': encoder.state_dict(),
+                    'de': decoder.state_dict(),
+                    'en_opt': encoder_optimizer.state_dict(),
+                    'de_opt': decoder_optimizer.state_dict(),
+                    'loss': loss,
+                    'voc_dict': voc.__dict__,
+                    'embedding': embedding.state_dict()
+                }, os.path.join(directory, '{}_{}.tar'.format(iteration, 'checkpoint')))
+
