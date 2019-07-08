573679e6853bbd0fdea421fe473b37355fb87e6f
Eunhyouk Shin <silvershine157@gmail.com>
2019-07-08 15:51:54 +0900

    finetuning cnn, eval

--- pytorch/tutorials/transfer_learning/main.py
+++ pytorch/tutorials/transfer_learning/main.py
@@ -19,14 +19,14 @@ plt.ion()
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.RandomResizedCrop(224), # hard?
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(256), 
+        transforms.CenterCrop(224), # easy?
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
@@ -46,20 +46,21 @@ dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def imsave(inp, title):
+def imsave(inp, fname, title=None):
     inp = inp.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
     inp = np.clip(inp, 0, 1)
     plt.imshow(inp)
-    plt.title(title)
-    plt.savefig('data/visual/'+str(title)+'.png')    
+    if title != None:
+        plt.title(title)
+    plt.savefig('data/visual/'+fname+'.png')    
     plt.pause(0.001)
 
 inputs, classes = next(iter(dataloaders['train']))
 out = torchvision.utils.make_grid(inputs)
-imsave(out, title=[class_names[x] for x in classes])
+imsave(out, fname='training_samples', title=[class_names[x] for x in classes])
 
 
 ## Training model
@@ -88,7 +89,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
                 labels = labels.to(device)
                 optimizer.zero_grad()
 
-                with torch.set_grad_enabled(phase=='train'):
+                with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
@@ -119,4 +120,51 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
+# display predictions for few images
+
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imsave(inputs.cpu().data[j], fname='visualize_model')
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
+
+
+## Finetuning convnet
+
+model_ft = models.resnet18(pretrained=True) # from torchvision
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 2) # replace fc layer
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+
+## train and eval
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+visualize_model(model_ft)
 
