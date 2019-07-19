df3a79206e7ca8a9580d179680d0fbbd19786ca2
Eunhyouk Shin <silvershine157@gmail.com>
2019-07-19 16:10:33 +0900

    factor out paths

--- pytorch/training/show_attend_tell/main.py
+++ pytorch/training/show_attend_tell/main.py
@@ -16,27 +16,8 @@ from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-## Data path
-
-flickr8k_paths = {
-	"data_dir": "./data/flickr8k/",
-	"orig_caption_file": "./data/flickr8k/Flickr_Data/Flickr_TextData/Flickr8k.lemma.token.txt",
-	"orig_image_dir": "./data/flickr8k/Flickr_Data/Images/",
-	"intermediate_data_file": "./data/flickr8k/processed/intermediate_data",
-	"cnn_activations_file": "./data/flickr8k/processed/cnn_activations",
-	"ckpt_dir": "./data/flickr8k/ckpt/"
-}
-
-toy_data_basic_paths = {
-	"data_dir": "./data/toy_data/basic/",
-	"orig_caption_file": "./data/toy_data/basic/captions.txt",
-	"orig_image_dir": "./data/toy_data/basic/Images/",
-	"intermediate_data_file": "./data/toy_data/basic/processed/intermediate_data",
-	"cnn_activations_file": "./data/toy_data/basic/processed/cnn_activations",
-	"ckpt_dir": "./data/toy_data/basic/ckpt/"
-}
-
-PATHS = flickr8k_paths
+## Data paths
+from paths import flickr8k_paths as PATHS
 
 ## Preprocessing options
 NEW_INTERMEDIATE_DATA = False # voc, captions, image names
---
+++ pytorch/training/show_attend_tell/paths.py
@@ -0,0 +1,18 @@
+
+flickr8k_paths = {
+	"data_dir": "./data/flickr8k/",
+	"orig_caption_file": "./data/flickr8k/Flickr_Data/Flickr_TextData/Flickr8k.lemma.token.txt",
+	"orig_image_dir": "./data/flickr8k/Flickr_Data/Images/",
+	"intermediate_data_file": "./data/flickr8k/processed/intermediate_data",
+	"cnn_activations_file": "./data/flickr8k/processed/cnn_activations",
+	"ckpt_dir": "./data/flickr8k/ckpt/"
+}
+
+toy_data_basic_paths = {
+	"data_dir": "./data/toy_data/basic/",
+	"orig_caption_file": "./data/toy_data/basic/captions.txt",
+	"orig_image_dir": "./data/toy_data/basic/Images/",
+	"intermediate_data_file": "./data/toy_data/basic/processed/intermediate_data",
+	"cnn_activations_file": "./data/toy_data/basic/processed/cnn_activations",
+	"ckpt_dir": "./data/toy_data/basic/ckpt/"
+}
