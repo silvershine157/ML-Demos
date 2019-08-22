81e1bad8a8baeab325be4b61c79a85fadbe8d88e
Eunhyouk Shin <silvershine157@gmail.com>
2019-08-22 16:36:23 +0900

    oneshot dataset, dataloader

--- pytorch/training/relnet_fsl/main.py
+++ pytorch/training/relnet_fsl/main.py
@@ -1,6 +1,10 @@
 import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from skimage import io
 import os
 import random
+import numpy as np
 
 
 '''
@@ -12,8 +16,17 @@ Meta test set
 - Test set
 '''
 
-class RelationNet():
-	pass
+EPISODES = 3
+
+class RelationNet(nn.Module):
+	'''A dummy implementation of relation net'''
+	def __init__(self):
+		super(RelationNet, self).__init__()
+		self.sigmoid = nn.Sigmoid()
+
+	def forward(img1, img2):
+		return self.sigmoid(torch.mean(img1+img2))
+
 
 def get_class_dirs():
 
@@ -40,12 +53,46 @@ def get_class_dirs():
 
 	return meta_train_dirs, meta_val_dirs, meta_test_dirs
 
-def train_episode(net, meta_train_dirs):
 
-	pass
+class OmniglotOneshotDataset(Dataset):
+	def __init__(self, dir_list):
+		self.dir_list = dir_list
 
-def main():
+	def __len__(self):
+		return len(self.dir_list)
+
+	def __getitem__(self, idx):
+		img_dir = self.dir_list[idx]
+		imgs = []
+		for img_name in os.listdir(img_dir):
+			img = io.imread(os.path.join(img_dir, img_name))
+			img = torch.from_numpy(img)
+			img = img.unsqueeze(dim=0)
+			img = img/255
+			imgs.append(img)
+		random.shuffle(imgs)
+		sample = imgs.pop()
+		query = torch.cat(imgs, dim=0)
+		return {"sample":sample, "query":query}
+
+def test():
+	# one pass
 	meta_train_dirs, meta_val_dirs, meta_test_dirs = get_class_dirs()
 	net = RelationNet()
+	dataset = OmniglotOneshotDataset(meta_train_dirs)
 
-main()
+	dataloader = DataLoader(dataset, batch_size=5, shuffle=True)
+
+	for episode in range(EPISODES):
+
+		ep_data = next(iter(dataloader))
+		sample = ep_data['sample']
+		query = ep_data['query']
+
+
+		
+
+def test2():
+	pass
+
+test()
