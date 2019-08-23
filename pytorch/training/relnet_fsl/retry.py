0c030f610ee366b41621256ffeca6fe04b901990
Eunhyouk Shin <silvershine157@gmail.com>
2019-08-23 18:08:28 +0900

    to debug: learns to predict equal scores for everything

--- pytorch/training/relnet_fsl/retry.py
+++ pytorch/training/relnet_fsl/retry.py
@@ -10,25 +10,27 @@ import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-'''
-Meta train set
-- Sample set
-- Query set
-Meta test set
-- Support set
-- Test set
-'''
 
+CLASS_IN_EP = 3
+QUERY_SIZE = 4
 EPISODES = 1000
-LEARNING_RATE = 0.001
-VALIDATE_EVERY = 100
-VALIDATE_EPS = 25
-PRINT_EVERY = 50
+PRINT_EVERY = 200
+LEARNING_RATE = 0.1
 
-'''
-network architecture is identical to the author's code:
-https://github.com/floodsung/LearningToCompare_FSL/blob/master/omniglot/omniglot_train_one_shot.py
-'''
+class SimpleEmbeddingModule(nn.Module):
+	def __init__(self):
+		super(SimpleEmbeddingModule, self).__init__()
+		self.layer1 = nn.Sequential(
+			nn.Conv2d(1, 64, kernel_size=3, padding=0),
+			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.ReLU(),
+			nn.MaxPool2d(2)
+		)
+
+	def forward(self, img):
+		out = self.layer1(img)
+		return out
+
 class EmbeddingModule(nn.Module):
 	def __init__(self):
 		super(EmbeddingModule, self).__init__()
@@ -64,6 +66,21 @@ class EmbeddingModule(nn.Module):
 		out = self.layer2(out)
 		out = self.layer3(out)
 		out = self.layer4(out)		
+		return out
+
+class SimpleRelationModule(nn.Module):
+	def __init__(self):
+		super(SimpleRelationModule, self).__init__()
+		self.layer = nn.Sequential(
+			nn.Linear(128, 8),
+			nn.ReLU(),
+			nn.Linear(8, 1),
+			nn.Sigmoid()
+		)
+
+	def forward(self, combined):
+		out = combined.view(combined.size(0), -1)
+		out = self.layer(out)
 		return out
 
 class RelationModule(nn.Module):
@@ -146,24 +163,9 @@ class OmniglotOneshotDataset(Dataset):
 			imgs.append(img)
 		random.shuffle(imgs)
 		sample = imgs.pop()
+		imgs = imgs[:QUERY_SIZE]
 		query = torch.cat(imgs, dim=0)
 		return {"sample":sample, "query":query}
-
-def weights_init(m):
-	classname = m.__class__.__name__
-	if classname.find('Conv') != -1:
-		n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-		m.weight.data.normal_(0, math.sqrt(2. / n))
-		if m.bias is not None:
-			m.bias.data.zero_()
-		elif classname.find('BatchNorm') != -1:
-			m.weight.data.fill_(1)
-			m.bias.data.zero_()
-		elif classname.find('Linear') != -1:
-			n = m.weight.size(1)
-			m.weight.data.normal_(0, 0.01)
-			m.bias.data = torch.ones(m.bias.data.size())
-
 
 
 def combine_pairs(sample_features, query_features):
@@ -171,20 +173,20 @@ def combine_pairs(sample_features, query_features):
 	_, C, D, _ = sample_features.size()
 	
 	# generate labels
-	sample_classes = torch.arange(5) # 5
-	query_classes = sample_classes.unsqueeze(dim=1).expand(-1, 19) # 5 x 19
+	sample_classes = torch.arange(CLASS_IN_EP) # 5
+	query_classes = sample_classes.unsqueeze(dim=1).expand(-1, QUERY_SIZE) # 5 x 19
 
 	# expand dimensions
-	sample_classes = sample_classes.unsqueeze(dim=1).expand(-1, 95) # 5 x 95	
-	query_classes = query_classes.contiguous().view(1, -1).expand(5, -1) # 5 x 95
+	sample_classes = sample_classes.unsqueeze(dim=1).expand(-1, QUERY_SIZE*CLASS_IN_EP) # 5 x 95	
+	query_classes = query_classes.contiguous().view(1, -1).expand(CLASS_IN_EP, -1) # 5 x 95
 	
 	# generate target
 	target = (sample_classes == query_classes).type(torch.FloatTensor)
 	target = target.view(-1) # 475
 
 	# expand for pairing
-	sample_features = sample_features.unsqueeze(dim=1).expand(-1, 95, -1, -1, -1) # 5 x 95 x C x D x D
-	query_features = query_features.unsqueeze(dim=0).expand(5, -1, -1, -1, -1) # 5 x 95 x C x D x D
+	sample_features = sample_features.unsqueeze(dim=1).expand(-1, QUERY_SIZE*CLASS_IN_EP, -1, -1, -1) # 5 x 95 x C x D x D
+	query_features = query_features.unsqueeze(dim=0).expand(CLASS_IN_EP, -1, -1, -1, -1) # 5 x 95 x C x D x D
 	
 	# concat in depth
 	combined = torch.cat([sample_features, query_features], dim=2) # 5 x 95 x 2C x D x D
@@ -193,19 +195,37 @@ def combine_pairs(sample_features, query_features):
 	return combined, target
 
 
-def test():
 
-	# setup data
-	meta_train_dirs, meta_val_dirs, meta_test_dirs = get_class_dirs()
-	metatrain_dataset = OmniglotOneshotDataset(meta_train_dirs)
-	metatrain_loader = DataLoader(metatrain_dataset, batch_size=5, shuffle=True)
-	metaval_dataset = OmniglotOneshotDataset(meta_val_dirs)
-	metaval_loader = DataLoader(metaval_dataset, batch_size=5, shuffle=True)
+def test1():
+	# fixed episode
+	dirs, _, _ = get_class_dirs()
+	dataset = OmniglotOneshotDataset(dirs)
+	loader = DataLoader(dataset, batch_size=CLASS_IN_EP, shuffle=True)
+	ep_data = next(iter(loader))
+	sample = ep_data['sample'] # CLASS_IN_EP x 1 x 28 x 28
+	query = ep_data['query'] # CLASS_IN_EP x QUERY_SIZE x 28 x 28
 	
-	# construct model
-	print('Constructing model . . .')
+	# use dummy data
+	sample = torch.arange(CLASS_IN_EP).type(torch.FloatTensor)
+	sample = sample.unsqueeze(dim=1).unsqueeze(dim=2).unsqueeze(dim=3)
+	sample = sample.expand(-1, -1, 28, 28).contiguous()
+	query = sample.expand(-1, QUERY_SIZE, -1, -1).contiguous()
+	query = query.view(-1, 1, 28, 28)
+
+	combined_raw, target = combine_pairs(sample, query)
+	print(combined_raw.shape)
+	print(combined_raw[:, :, 1, 1])
+	print(target.shape)
+	print(target)
+
 	embed_net = EmbeddingModule()
 	rel_net = RelationModule()
+	# use simple network
+	#embed_net = SimpleEmbeddingModule()
+	print(query.shape)
+	query_feature = embed_net(query)
+	print(query_feature.shape)
+
 	criterion = nn.MSELoss()
 
 	# setup training
@@ -214,34 +234,13 @@ def test():
 	embed_scheduler = StepLR(embed_opt, step_size=100000, gamma=0.5)
 	rel_scheduler = StepLR(rel_opt, step_size=100000, gamma=0.5)
 
-	embed_net.apply(weights_init)
-	rel_net.apply(weights_init)
-	embed_net.to(device)
-	rel_net.to(device)
-
-	# training
-	print('Training . . .')
 	running_loss = 0.0
 	for episode in range(1, EPISODES+1):
-		
-		embed_scheduler.step(episode)
-		rel_scheduler.step(episode)
-
-		# form episode
-		ep_data = next(iter(metatrain_loader))
-		sample = ep_data['sample'].to(device) # 5 x 1 x 28 x 28
-		query = ep_data['query'].to(device) # 5 x 19 x 28 x 28
-		query = query.view(-1, 1, 28, 28) # flattening, 95 x 1 x 28 x 28
-
-		# forward pass
-		sample_features = embed_net(sample) # 5 x C x D x D (avoid redundant computation)
-		query_features = embed_net(query) # 95 x C x D x D
+		sample_features = embed_net(sample)
+		query_features = embed_net(query)
 		combined, score_target = combine_pairs(sample_features, query_features)
-		score_target = score_target.to(device)
 		score_pred = rel_net(combined)
 		loss = criterion(score_pred, score_target)
-
-		# backward pass & update
 		embed_net.zero_grad()
 		rel_net.zero_grad()
 		loss.backward()
@@ -249,37 +248,66 @@ def test():
 		nn.utils.clip_grad_norm_(rel_net.parameters(), 0.5)
 		embed_opt.step()
 		rel_opt.step()
-
 		running_loss += loss.item()
 		if (episode % PRINT_EVERY == 0):
 			print('episode: ', episode, 'loss: ', running_loss/PRINT_EVERY)
 			running_loss = 0.0
-		
-		if (episode % VALIDATE_EVERY == 0):
-			correct = 0
-			for _ in range(VALIDATE_EPS):
-				ep_data = next(iter(metaval_loader))
-				sample = ep_data['sample'].to(device)
-				query = ep_data['query'].to(device)
-				query = query.view(-1, 1, 28, 28)
-				sample_features = embed_net(sample)
-				query_features = embed_net(query)
-				combined, score_target = combine_pairs(sample_features, query_features)
-				score_target = score_target.to(device)
-				# combined: 475 x 2C x D x D
-				# target: 475
-				score_pred = rel_net(combined) # 475
-				target_class = torch.argmax(score_target.view(5, 95), dim=0)
-				pred_class = torch.argmax(score_pred.view(5, 95), dim=0)
-				equals = (target_class == pred_class)
-				correct += torch.sum(equals).item()
+			'''
+			print('target')
+			print(score_target.view(-1).numpy())
+			print('pred')
+			print(score_pred.detach().view(-1).numpy())
+			'''
 
-			accuracy = correct/95/VALIDATE_EPS
-			print('validation accuracy: ', accuracy)
+	print(score_pred.detach().numpy())
 
 
-# TODO: validation (preferably modularized), monitoring log, good initlialization, better target, learning rate
-# schedulig
+def test2():
+	# only train relation module
+
+	# use dummy data
+	sample = torch.arange(CLASS_IN_EP).type(torch.FloatTensor)
+	sample = sample.unsqueeze(dim=1).unsqueeze(dim=2).unsqueeze(dim=3)
+	sample = sample.expand(-1, -1, 28, 28).contiguous()
+	query = sample.expand(-1, QUERY_SIZE, -1, -1).contiguous()
+	query = query.view(-1, 1, 28, 28)
+	sample_features = (sample.expand(-1, 64, -1, -1))[:, :, 0:1, 0:1]
+	query_features = (query.expand(-1, 64, -1, -1))[:, :, 0:1, 0:1]
+	print(sample_features.shape)
+	print(query_features.shape)	
+	combined, score_target = combine_pairs(sample_features, query_features)
+
+	rel_net = RelationModule()
+	# use simple network
+	rel_net = SimpleRelationModule()
+
+	criterion = nn.MSELoss()
+
+	# setup training
+	rel_opt = torch.optim.Adam(rel_net.parameters(), lr=LEARNING_RATE)
+	
+	running_loss = 0.0
+	for episode in range(1, EPISODES+1):
+		score_pred = rel_net(combined)
+		loss = criterion(score_pred, score_target)
+		rel_net.zero_grad()
+		loss.backward()
+		rel_opt.step()
+		running_loss += loss.item()
+		if (episode % PRINT_EVERY == 0):
+			print('episode: ', episode, 'loss: ', running_loss/PRINT_EVERY)
+			running_loss = 0.0
+			'''
+			print('target')
+			print(score_target.view(-1).numpy())
+			print('pred')
+			print(score_pred.detach().view(-1).numpy())
+			'''
+
+	print(combined[:, 0, 0, 0].view(CLASS_IN_EP, -1).numpy())
+	print(combined[:, 64, 0, 0].view(CLASS_IN_EP, -1).numpy())
+	print(score_target.view(CLASS_IN_EP, -1).numpy())
+	print(score_pred.view(CLASS_IN_EP, -1).detach().numpy())
 
 
-test()
+test2()
