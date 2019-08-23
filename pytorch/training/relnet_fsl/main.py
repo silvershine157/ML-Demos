10f0004882224eff16c6fc791070c9cdf7307cfb
Eunhyouk Shin <silvershine157@gmail.com>
2019-08-23 20:14:33 +0900

    setup configuration, ready to submit

--- pytorch/training/relnet_fsl/main.py
+++ pytorch/training/relnet_fsl/main.py
@@ -10,15 +10,21 @@ import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-TRAIN_EPISODES = 100
+## Global config parameters
+
+TRAIN_EPISODES = 3000
 LEARNING_RATE = 0.001
-VALIDATE_EVERY = 20
-VAL_EPISODES = 25
-TEST_EPISODES = 100
-PRINT_EVERY = 10
+VALIDATE_EVERY = 300
+VAL_EPISODES = 50
+TEST_EPISODES = 200
+PRINT_EVERY = 100
 CLASS_IN_EP = 5
 QUERY_SIZE = 19
 FEATURE_DIM = 64
+HIDDEN_SIZE = 8
+
+
+## Model
 
 '''
 network architecture and training detail is identical to the author's code:
@@ -30,31 +36,31 @@ class EmbeddingModule(nn.Module):
 
 		# define architecture
 		self.layer1 = nn.Sequential(
-			nn.Conv2d(1, 64, kernel_size=3, padding=0),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(1, FEATURE_DIM, kernel_size=3, padding=0),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU(),
 			nn.MaxPool2d(2)
 		)
 		self.layer2 = nn.Sequential(
-			nn.Conv2d(64, 64, kernel_size=3, padding=0),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(FEATURE_DIM, FEATURE_DIM, kernel_size=3, padding=0),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU(),
 			nn.MaxPool2d(2)
 		)
 		self.layer3 = nn.Sequential(
-			nn.Conv2d(64, 64, kernel_size=3, padding=0),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(FEATURE_DIM, FEATURE_DIM, kernel_size=3, padding=0),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU()
 		)
 		self.layer4 = nn.Sequential(
-			nn.Conv2d(64, 64, kernel_size=3, padding=0),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(FEATURE_DIM, FEATURE_DIM, kernel_size=3, padding=0),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU(),
 		)
 
 	def forward(self, img):
 		# img: B x 1 x 28 x 28
-		# out: B x C x D x D
+		# out: B x FEATURE_DIM x 1 x 1
 		out = self.layer1(img)
 		out = self.layer2(out)
 		out = self.layer3(out)
@@ -65,36 +71,82 @@ class RelationModule(nn.Module):
 	def __init__(self):
 		super(RelationModule, self).__init__()
 		
-		input_size = 64
-		hidden_size = 8
 		# define architecture
 		self.layer1 = nn.Sequential(
-			nn.Conv2d(128, 64, kernel_size=3, padding=1),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(2*FEATURE_DIM, FEATURE_DIM, kernel_size=3, padding=1),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU(),
 			nn.MaxPool2d(2)
 		)
 		self.layer2 = nn.Sequential(
-			nn.Conv2d(64, 64, kernel_size=3, padding=1),
-			nn.BatchNorm2d(64, momentum=1, affine=True),
+			nn.Conv2d(FEATURE_DIM, FEATURE_DIM, kernel_size=3, padding=1),
+			nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
 			nn.ReLU(),
 			nn.MaxPool2d(2)
 		)
 		self.out_layer = nn.Sequential(
-			nn.Linear(input_size, hidden_size),
+			nn.Linear(FEATURE_DIM, HIDDEN_SIZE),
 			nn.ReLU(),
-			nn.Linear(hidden_size, 1),
+			nn.Linear(HIDDEN_SIZE, 1),
 			nn.Sigmoid()
 		)
 
 	def forward(self, combined):
-		# combined: B x 2C x D x D
-		# out: B
+		# combined: B x (2*FEATURE_DIM) x 1 x 1
+		# out: B x 1
 		out = self.layer1(combined)
 		out = self.layer2(out)
 		out = out.view(out.size(0), -1)
 		out = self.out_layer(out)
 		return out
+
+def combine_pairs(sample_features, query_features):
+
+	flat_size = CLASS_IN_EP * QUERY_SIZE
+	num_pairs = CLASS_IN_EP * flat_size
+
+	# generate labels
+	sample_classes = torch.arange(CLASS_IN_EP) # 5
+	query_classes = sample_classes.unsqueeze(dim=1).expand(-1, QUERY_SIZE) # CLASS_IN_EP x QUERY_SIZE
+
+	# expand dimensions
+	sample_classes = sample_classes.unsqueeze(dim=1).expand(-1, flat_size) # CLASS_IN_EP x flat_size	
+	query_classes = query_classes.contiguous().view(1, -1).expand(5, -1) # CLASS_IN_EP x flat_size
+	
+	# generate target
+	target = (sample_classes == query_classes).type(torch.FloatTensor)
+	target = target.view(num_pairs, 1)
+
+	# expand for pairing
+	# both will have size: CLASS_IN_EP X flat_size x FEATURE_DIM x 1 x 1
+	sample_features = sample_features.unsqueeze(dim=1).expand(-1, flat_size, -1, -1, -1)
+	query_features = query_features.unsqueeze(dim=0).expand(CLASS_IN_EP, -1, -1, -1, -1)
+
+	# concat in depth
+	combined = torch.cat([sample_features, query_features], dim=2)
+	combined = combined.view(num_pairs, 2*FEATURE_DIM, 1, 1)
+	# combined: num_pairs x (2*FEATURE_DIM) x 1 x 1
+	
+	return combined, target
+
+
+def weights_init(m):
+	classname = m.__class__.__name__
+	if classname.find('Conv') != -1:
+		n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+		m.weight.data.normal_(0, math.sqrt(2. / n))
+		if m.bias is not None:
+			m.bias.data.zero_()
+		elif classname.find('BatchNorm') != -1:
+			m.weight.data.fill_(1)
+			m.bias.data.zero_()
+		elif classname.find('Linear') != -1:
+			n = m.weight.size(1)
+			m.weight.data.normal_(0, 0.01)
+			m.bias.data = torch.ones(m.bias.data.size())
+
+
+## Data helpers
 
 def get_class_dirs():
 
@@ -121,7 +173,6 @@ def get_class_dirs():
 
 	return metatrain_dirs, metaval_dirs, metatest_dirs
 
-
 class OmniglotOneshotDataset(Dataset):
 	def __init__(self, dir_list):
 		self.dir_list = dir_list
@@ -144,67 +195,32 @@ class OmniglotOneshotDataset(Dataset):
 		query = torch.cat(imgs, dim=0)
 		return {"sample":sample, "query":query}
 
-def weights_init(m):
-	classname = m.__class__.__name__
-	if classname.find('Conv') != -1:
-		n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-		m.weight.data.normal_(0, math.sqrt(2. / n))
-		if m.bias is not None:
-			m.bias.data.zero_()
-		elif classname.find('BatchNorm') != -1:
-			m.weight.data.fill_(1)
-			m.bias.data.zero_()
-		elif classname.find('Linear') != -1:
-			n = m.weight.size(1)
-			m.weight.data.normal_(0, 0.01)
-			m.bias.data = torch.ones(m.bias.data.size())
 
-
-
-def combine_pairs(sample_features, query_features):
-
-	_, C, D, _ = sample_features.size()
-	
-	# generate labels
-	sample_classes = torch.arange(5) # 5
-	query_classes = sample_classes.unsqueeze(dim=1).expand(-1, 19) # 5 x 19
-
-	# expand dimensions
-	sample_classes = sample_classes.unsqueeze(dim=1).expand(-1, 95) # 5 x 95	
-	query_classes = query_classes.contiguous().view(1, -1).expand(5, -1) # 5 x 95
-	
-	# generate target
-	target = (sample_classes == query_classes).type(torch.FloatTensor)
-	target = target.view(-1, 1) # 475 x 1
-
-	# expand for pairing
-	sample_features = sample_features.unsqueeze(dim=1).expand(-1, 95, -1, -1, -1) # 5 x 95 x C x D x D
-	query_features = query_features.unsqueeze(dim=0).expand(5, -1, -1, -1, -1) # 5 x 95 x C x D x D
-	
-	# concat in depth
-	combined = torch.cat([sample_features, query_features], dim=2) # 5 x 95 x 2C x D x D
-	combined = combined.view(-1, 2*C, D, D) # 475 x 2C x D x D
-	
-	return combined, target
-
+## Train/test routines
 
 def evaluate_accuracy(embed_net, rel_net, test_episodes, metatest_loader):
+
 	correct = 0
-	for _ in range(test_episodes):
-		ep_data = next(iter(metatest_loader))
-		sample = ep_data['sample'].to(device)
-		query = ep_data['query'].to(device)
-		query = query.view(-1, 1, 28, 28)
-				
-		sample_features = embed_net(sample)		
-		query_features = embed_net(query)
-		combined, score_target = combine_pairs(sample_features, query_features)
-		score_target = score_target.to(device)
-		score_pred = rel_net(combined)
-		class_target = torch.argmax(score_target.view(CLASS_IN_EP, CLASS_IN_EP * QUERY_SIZE), dim=0)
-		class_pred = torch.argmax(score_pred.view(CLASS_IN_EP, CLASS_IN_EP * QUERY_SIZE), dim=0)
-		equals = (class_target == class_pred)
-		correct += torch.sum(equals).item()
+	with torch.no_grad():
+		for _ in range(test_episodes):
+			# setup episode
+			ep_data = next(iter(metatest_loader))
+			sample = ep_data['sample'].to(device)
+			query = ep_data['query'].to(device)
+			query = query.view(-1, 1, 28, 28)
+			
+			# forward pass
+			sample_features = embed_net(sample)		
+			query_features = embed_net(query)
+			combined, score_target = combine_pairs(sample_features, query_features)
+			score_target = score_target.to(device)
+			score_pred = rel_net(combined)
+			class_target = torch.argmax(score_target.view(CLASS_IN_EP, CLASS_IN_EP * QUERY_SIZE), dim=0)
+			class_pred = torch.argmax(score_pred.view(CLASS_IN_EP, CLASS_IN_EP * QUERY_SIZE), dim=0)
+			
+			# count correct predictions
+			equals = (class_target == class_pred)
+			correct += torch.sum(equals).item()
 	
 	accuracy = correct/CLASS_IN_EP/QUERY_SIZE/test_episodes
 	return accuracy
@@ -245,13 +261,13 @@ def main():
 
 		# setup episode
 		ep_data = next(iter(metatrain_loader))
-		sample = ep_data['sample'].to(device) # 5 x 1 x 28 x 28
-		query = ep_data['query'].to(device) # 5 x 19 x 28 x 28
-		query = query.view(-1, 1, 28, 28) # flattening, 95 x 1 x 28 x 28
+		sample = ep_data['sample'].to(device) # CLASS_IN_EP x 1 x 28 x 28
+		query = ep_data['query'].to(device) # CLASS_IN_EP x QUERY_SIZE x 28 x 28
+		query = query.view(-1, 1, 28, 28) # flat_size x 1 x 28 x 28
 
 		# forward pass
-		sample_features = embed_net(sample) # 5 x C x D x D (avoid redundant computation)
-		query_features = embed_net(query) # 95 x C x D x D
+		sample_features = embed_net(sample) # CLASS_IN_EP x FEATURE_DIM x 1 x 1 (avoid redundant computation)
+		query_features = embed_net(query) # flat_size x FEATURE_DIM x 1 x 1
 		combined, score_target = combine_pairs(sample_features, query_features)
 		score_target = score_target.to(device)
 		score_pred = rel_net(combined)
@@ -265,14 +281,14 @@ def main():
 		embed_opt.step()
 		rel_opt.step()
 
+		# print progress
 		running_loss += loss.item()
 		if episode % PRINT_EVERY == 0:
-			# print progress
 			print('Episode %d, avg loss: %f'%(episode, running_loss/PRINT_EVERY))
 			running_loss = 0.0
 
+		# validate model
 		if episode % VALIDATE_EVERY == 0:
-			# validate model
 			val_accuracy = evaluate_accuracy(embed_net, rel_net, VAL_EPISODES, metaval_loader)
 			print('Validation accuracy: %f'%(val_accuracy))
 
@@ -280,6 +296,7 @@ def main():
 	print("Testing . . .")
 	test_accuracy = evaluate_accuracy(embed_net, rel_net, TEST_EPISODES, metatest_loader)
 	print('Test accuracy: %f'%(test_accuracy))
+
 
 
 main()
