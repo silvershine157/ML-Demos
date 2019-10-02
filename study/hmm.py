30c6f590b4a8530268c43a66e5884f7d766a8d8a
Eunhyuk Shin <silvershine157@gmail.com>
2019-10-02 14:35:51 +0900

    visualize 2D HMM data

--- study/hmm.py
+++ study/hmm.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
 ## model dimensions
 K = 3 # number of latent states
-N = 10 # number of data points
+N = 100 # number of data points
 D = 2 # dimension of a data point
 
 def generate_data():
@@ -12,20 +13,20 @@ def generate_data():
 	PI = np.array([0.2, 0.5, 0.3])
 	# p(z_n | z_{n-1}): transition matrix
 	A = np.array([
-		[0.2, 0.3, 0.5],
-		[0.5, 0.2, 0.3],
-		[0.3, 0.5, 0.2]
+		[0.7, 0.1, 0.2],
+		[0.3, 0.5, 0.2],
+		[0.2, 0.1, 0.7]
 	])
 	# p(x_n | z_n): D-dim gaussian
 	PHI_MU = np.array([
-		[0.0, 0.0],
-		[1.0, 1.0],
-		[-1.0, 1.0]
+		[0.3, 0.7],
+		[0.3, 0.2],
+		[0.7, 0.5]
 	])
-	PHI_SIGMA = np.array([
-		[[0.5, 0.0], [0.0, 0.5]],
-		[[0.5, 0.0], [0.0, 0.5]],
-		[[0.5, 0.0], [0.0, 0.5]]
+	PHI_SIGMA = 0.01*np.array([
+		[[0.5, 0.4], [0.4, 0.5]],
+		[[0.5, 0.0], [0.0, 0.2]],
+		[[0.5, -0.4], [-0.4, 0.5]]
 	])
 	## generate data
 	z = np.zeros((N), dtype=np.int16)
@@ -39,12 +40,18 @@ def generate_data():
 	return x, z
 
 def visualize_data(x, z=None):
-	if z:
+	if z is not None:
 		# color code latent variable
-		pass
+		colors = ['red', 'green', 'blue']
+		plt.plot(x[:, 0], x[:, 1], color='black', zorder=1)
+		plt.scatter(x[:, 0], x[:, 1], 
+			c=z, cmap=matplotlib.colors.ListedColormap(colors), zorder=2)
+		plt.show()
 	else:
 		# no color
-		pass
+		plt.plot(x[:, 0], x[:, 1])
+		plt.scatter(x[:, 0], x[:, 1])
+		plt.show()
 
 
 def main():
