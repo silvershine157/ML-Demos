3c7ca9bdbcd0cdfa8dacd917ea777009e92418a0
Eunhyuk Shin <silvershine157@gmail.com>
2019-11-28 13:38:16 +0900

    experiment module with hierarchical experiments

---
+++ experiment_module/expr.py
@@ -0,0 +1,77 @@
+import os
+
+class Experiment():
+
+	def __init__(self, args=None):
+		self.sub_exprs = []
+		self.args = args
+		self.local_id = self.make_local_id()
+		# self.add_exprs(SubExperiment(sub_args))
+
+	def make_local_id(self):
+		return "default_id"
+
+	def add_expr(self, expr):
+		self.sub_exprs.append(expr)
+
+	def run(self, log_dir):
+		sub_results = []
+		for sub_expr in self.sub_exprs:
+			sub_dir = os.join(log_dir, self.local_id)
+			os.makedirs(sub_dir)
+			sub_res = sub_expr.run(sub_dir)
+			results.append(sub_res)
+		result = self.produce_result(sub_results, log_dir)
+		return result
+
+	def produce_result(self, sub_results, log_dir):
+		result = None
+		# do experiment, write log
+		return result
+
+
+# Example experiment: tune lambda for MLP on MNIST
+class L2RegTuningExp(Experiment):
+
+	def __init__(self, min_lmbda, factor, n_lambdas):
+		args = {
+			"min_lmbda":min_lmbda,
+			"factor":factor,
+			"n_lambdas": n_lambdas
+		}
+		super(RegularizationExp, self).__init__(args)
+		for n in range(n_lambdas):
+			lmbda = 0.0
+			sub_expr = MNISTExp(lmbda)
+			self.add_expr(sub_expr)
+
+	def produce_result(self, sub_results, log_dir):
+		result = []
+		for sub_res in sub_results:
+			lmbda = sub_res["lmbda"]
+			test_accuracy = sub_res["test_accruacy"]
+			result.append((lmbda, test_accruacy))
+		# write log
+		return result
+
+
+class MNISTExp(Experiment):
+
+	def __init__(self, lmbda):
+		args = {
+			"lmbda": lmbda
+		}
+		super(MNISTExp, self).__init__(args)
+		# no subexperiments
+
+	def produce_result(self, sub_results, log_dir):
+		result = {}
+		lmbda = self.args["lmbda"]
+
+		# train and test with lmbda, write log
+
+		test_accuracy = 0.0
+		result["lmbda"] = lmbda
+		result["test_accuracy"] = test_accuracy
+		# write log
+		return result
