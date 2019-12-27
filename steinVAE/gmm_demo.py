32927ebc22ccd86fdb16f461a5e39906c916e907
Eunhyuk Shin <silvershine157@gmail.com>
2019-12-27 15:14:36 +0900

    batch processing, log joint

--- steinVAE/gmm_demo.py
+++ steinVAE/gmm_demo.py
@@ -2,39 +2,45 @@ import torch
 import numpy as np
 from torch.distributions.multivariate_normal import MultivariateNormal
 
-
+mu1 = torch.FloatTensor([5, 5])
+mu2 = torch.FloatTensor([-5, -5])
+theta = torch.FloatTensor([[2., -1.], [1., -2.]])
 
 def generate_data():
 	N_data = 100
-	mu1 = torch.FloatTensor([5, 5]).view(2, 1)
-	mu2 = torch.FloatTensor([-5, -5]).view(2, 1)
 	X = torch.zeros(2, N_data)
+	Z = torch.zeros(2, N_data)
 	for n in range(N_data):
 		if np.random.rand() < 0.5:
-			z = mu1 + torch.randn(2, 1)
+			z = mu1 + torch.randn(2)
 		else:
-			z = mu2 + torch.randn(2, 1)
-		mvn = dist_x_given_z(z)
-		x = mvn.sample()
-		X[:, n] = x
+			z = mu2 + torch.randn(2)
+		Z[:, n] = z
+	mean = torch.mm(theta, Z)
+	X = mean + 0.1*torch.randn(2, N_data)
 	return X
 
-def dist_x_given_z(z):
-	theta = torch.FloatTensor([[2., -1.], [1., -2.]])
-	mean = torch.mm(theta, z).t()
-	cov = 0.01*torch.eye(2)
-	mvn = MultivariateNormal(mean, cov)	
-	return mvn
+def log_prior(z):
+	lp_1 = MultivariateNormal(mu1, torch.eye(2)).log_prob(z)
+	lp_2 = MultivariateNormal(mu2, torch.eye(2)).log_prob(z)
+	log_pri = torch.log((torch.exp(lp_1)+torch.exp(lp_2))/2)
+	return log_pri
 
-def log_likelihood(z, x):
-	log_l = dist_x_given_z(z).log_prob(x)
+def log_likelihood(x, z):
+	mean = torch.mm(theta, z.view(2, -1)).view(-1, 2)
+	log_l = MultivariateNormal(mean, 0.01*torch.eye(2)).log_prob(z)
 	return log_l
+
+# unnormalized posterior
+def log_joint(z, x): 
+	log_j = log_likelihood(x, z) + log_prior(z)
+	return log_j
 
 def main():
 	X = generate_data()
-	z = torch.FloatTensor([0, 0]).view(2, 1)
+	z = torch.zeros(2)
 	z.requires_grad_(True)
-	log_l = log_likelihood(z, X[:, 0])
-	print(log_l)
+	log_j = log_joint(z, X[:, 0])
+	print(log_j)
 
 main()
