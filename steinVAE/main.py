485bfcee496cb0b4c641c232e74b7b4b8e383fa3
Eunhyuk Shin <silvershine157@gmail.com>
2019-12-27 13:53:12 +0900

    VAE rebalance loss terms

--- steinVAE/main.py
+++ steinVAE/main.py
@@ -9,31 +9,32 @@ import torch.optim as optim
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class Encoder(nn.Module):
+hidden_dim = 500
+
+class EncoderMLP(nn.Module):
 	def __init__(self, latent_dim):
-		super(Encoder, self).__init__()
+		super(EncoderMLP, self).__init__()
 		self.latent_dim = latent_dim
 		self.layers = nn.Sequential(
-			nn.Linear(28*28, 200),
+			nn.Linear(28*28, hidden_dim),
 			nn.Tanh(),
-			nn.Linear(200, latent_dim*2)
+			nn.Linear(hidden_dim, latent_dim*2)
 		)
 	def forward(self, x):
 		B = x.size(0)
 		out = self.layers(x.view(B, -1))
-		mu, pre_sigma = out[:, :self.latent_dim], out[:, self.latent_dim:]
-		sigmoid = nn.Sigmoid()
-		sigma = sigmoid(pre_sigma)
+		mu, log_sigma = out[:, :self.latent_dim], out[:, self.latent_dim:]
+		sigma = torch.exp(log_sigma)
 		return mu, sigma
 
-class Decoder(nn.Module):
+class DecoderMLP(nn.Module):
 	def __init__(self, latent_dim):
-		super(Decoder, self).__init__()
+		super(DecoderMLP, self).__init__()
 		self.latent_dim = latent_dim
 		self.layers = nn.Sequential(
-			nn.Linear(latent_dim, 200),
+			nn.Linear(latent_dim, hidden_dim),
 			nn.Tanh(),
-			nn.Linear(200, 28*28),
+			nn.Linear(hidden_dim, 28*28),
 			nn.Sigmoid()
 		)
 	def forward(self, z):
@@ -41,6 +42,7 @@ class Decoder(nn.Module):
 		out = self.layers(z)
 		x_r = out.view(B, 1, 28, 28)
 		return x_r
+
 
 def get_mnist_loaders(batch_size):
 	mnist_transform = transforms.Compose([
@@ -63,21 +65,21 @@ def get_mnist_loaders(batch_size):
 #plt.show()
 
 def D_KL(mu, sigma):
-	out = -torch.mean(1 + torch.log(sigma**2) - mu**2 - sigma**2)
+	out = -torch.sum(1 + torch.log(sigma**2) - mu**2 - sigma**2)
 	return out
 
 def main():
 
 	latent_dim=50
-	batch_size=32
-	lr = 0.001
+	batch_size=100
+	lr = 0.01
 	train_loader, valid_loader, test_loader = get_mnist_loaders(batch_size)
-	enc = Encoder(latent_dim).to(device)
-	dec = Decoder(latent_dim).to(device)
-	enc_optim = optim.Adam(enc.parameters(), lr=lr)
-	dec_optim = optim.Adam(dec.parameters(), lr=lr)
+	enc = EncoderMLP(latent_dim).to(device)
+	dec = DecoderMLP(latent_dim).to(device)
+	enc_optim = optim.Adagrad(enc.parameters(), lr=lr)
+	dec_optim = optim.Adagrad(dec.parameters(), lr=lr)
 
-	for epoch in range(10):
+	for epoch in range(30):
 		running_loss = 0.0
 		running_n = 0
 		for x, y in train_loader:
@@ -89,16 +91,16 @@ def main():
 			noise = torch.randn(B, latent_dim).to(device)
 			z = mu + sigma * noise
 			x_r = dec(z)
-			LL_0 = torch.mean(torch.log(1-x_r[x < 0.5]))
-			LL_1 = torch.mean(torch.log(x_r[x > 0.5]))
-			LL = (LL_0+LL_1)/2
+			LL_0 = torch.sum(torch.log(1-x_r[x < 0.5]))
+			LL_1 = torch.sum(torch.log(x_r[x > 0.5]))
+			LL = LL_0+LL_1
 			loss = -(LL - D_KL(mu, sigma))
 			loss.backward()
 			enc_optim.step()
 			dec_optim.step()
 			running_n += B
-			running_loss += B*loss.item()
-		print(running_loss/running_n)
+			running_loss += loss.item()
+		print(epoch, -running_loss/running_n)
 
 	enc.cpu()
 	dec.cpu()
