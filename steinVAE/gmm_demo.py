95b90952ba0ddd7d03f00ce3cf831248db1c0216
Eunhyuk Shin <silvershine157@gmail.com>
2019-12-27 16:26:25 +0900

    GMM posterior analytic

--- steinVAE/gmm_demo.py
+++ steinVAE/gmm_demo.py
@@ -31,7 +31,7 @@ def log_prior(z):
 
 def log_likelihood(x, z):
 	mean = torch.mm(theta, z.view(2, -1)).view(-1, 2)
-	log_l = MultivariateNormal(mean, torch.eye(2)).log_prob(x)
+	log_l = MultivariateNormal(mean, 0.01*torch.eye(2)).log_prob(x)
 	return log_l
 
 # unnormalized posterior
@@ -39,12 +39,21 @@ def log_joint(z, x):
 	log_j = log_likelihood(x, z) + log_prior(z)
 	return log_j
 
-def main():
-	X = generate_data(100)
-	z = torch.zeros(2)
-	z.requires_grad_(True)
-	log_j = log_joint(z, X[:, 0])
-	print(log_j)
+def log_joint_analytic(z, x):
+	sigma = 0.1
+	Sigma = torch.matmul(theta.t(), theta)/(sigma**2) + torch.eye(2)
+	SigmaInv = Sigma.inverse()
+	mu1hat = torch.matmul(SigmaInv,torch.matmul(x, theta)/(sigma**2)-mu1)
+	mu2hat = torch.matmul(SigmaInv,torch.matmul(x, theta)/(sigma**2)-mu2)
+	quad1 = torch.matmul(mu1hat, torch.matmul(Sigma, mu1hat))
+	quad2 = torch.matmul(mu2hat, torch.matmul(Sigma, mu2hat))
+	p1 = torch.sum(x**2)/(sigma**2) + torch.sum(mu1**2) - quad1
+	p2 = torch.sum(x**2)/(sigma**2) + torch.sum(mu2**2) - quad2
+	phat = 1/(1+torch.exp(p2-p1))
+	lp1 = MultivariateNormal(mu1hat, Sigma).log_prob(z)
+	lp2 = MultivariateNormal(mu2hat, Sigma).log_prob(z)
+	res = phat*torch.exp(lp1) + (1-phat)*torch.exp(lp2)
+	return torch.log(res)
 
 def posterior_visualization():
 	X = generate_data(1)
@@ -57,11 +66,23 @@ def posterior_visualization():
 	for z1_i, z1 in enumerate(z_grid):
 		for z2_i, z2 in enumerate(z_grid):
 			z = torch.Tensor([z1, z2])
-			val = log_joint(z, x)
+			val = log_joint_analytic(z, x)
 			log_j_values[z1_i, z2_i] = val
-	j_values = torch.exp(log_j_values)
+	j_values = torch.exp(log_j_values)/100
 	img = j_values.numpy()
 	plt.imshow(img)
 	plt.show()
+
+def test_data():
+	X = generate_data(100)
+	z = torch.zeros(2)
+	z.requires_grad_(True)
+	log_j = log_joint(z, X[:, 0])
+	print(log_j)
+
+def test_analytic():
+	x = torch.zeros(2)
+	z = torch.zeros(2)
+	log_j = log_joint_analytic(z, x)
 
 posterior_visualization()
