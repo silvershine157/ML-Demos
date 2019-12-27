51a24fd7775c6d31054c0d766246cb61afb13c2a
Eunhyuk Shin <silvershine157@gmail.com>
2019-12-27 15:44:08 +0900

    visualize posterior (mismatch with paper?)

--- steinVAE/gmm_demo.py
+++ steinVAE/gmm_demo.py
@@ -1,24 +1,27 @@
 import torch
 import numpy as np
 from torch.distributions.multivariate_normal import MultivariateNormal
+import matplotlib.pyplot as plt
 
 mu1 = torch.FloatTensor([5, 5])
 mu2 = torch.FloatTensor([-5, -5])
 theta = torch.FloatTensor([[2., -1.], [1., -2.]])
 
-def generate_data():
-	N_data = 100
+def generate_data(N_data):
 	X = torch.zeros(2, N_data)
 	Z = torch.zeros(2, N_data)
 	for n in range(N_data):
-		if np.random.rand() < 0.5:
-			z = mu1 + torch.randn(2)
-		else:
-			z = mu2 + torch.randn(2)
-		Z[:, n] = z
+		Z[:, n] = sample_z()
 	mean = torch.mm(theta, Z)
 	X = mean + 0.1*torch.randn(2, N_data)
 	return X
+
+def sample_z():
+	if np.random.rand() < 0.5:
+		z = mu1 + torch.randn(2)
+	else:
+		z = mu2 + torch.randn(2)
+	return z	
 
 def log_prior(z):
 	lp_1 = MultivariateNormal(mu1, torch.eye(2)).log_prob(z)
@@ -28,7 +31,7 @@ def log_prior(z):
 
 def log_likelihood(x, z):
 	mean = torch.mm(theta, z.view(2, -1)).view(-1, 2)
-	log_l = MultivariateNormal(mean, 0.01*torch.eye(2)).log_prob(z)
+	log_l = MultivariateNormal(mean, torch.eye(2)).log_prob(x)
 	return log_l
 
 # unnormalized posterior
@@ -37,10 +40,28 @@ def log_joint(z, x):
 	return log_j
 
 def main():
-	X = generate_data()
+	X = generate_data(100)
 	z = torch.zeros(2)
 	z.requires_grad_(True)
 	log_j = log_joint(z, X[:, 0])
 	print(log_j)
 
-main()
+def posterior_visualization():
+	X = generate_data(1)
+	x = X[:, 0]
+	x = torch.zeros(2)
+	n_grid = 50
+	grid_size = 7.
+	z_grid = np.linspace(-grid_size, grid_size, n_grid)
+	log_j_values = torch.zeros((n_grid, n_grid))
+	for z1_i, z1 in enumerate(z_grid):
+		for z2_i, z2 in enumerate(z_grid):
+			z = torch.Tensor([z1, z2])
+			val = log_joint(z, x)
+			log_j_values[z1_i, z2_i] = val
+	j_values = torch.exp(log_j_values)
+	img = j_values.numpy()
+	plt.imshow(img)
+	plt.show()
+
+posterior_visualization()
