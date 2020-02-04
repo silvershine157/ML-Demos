0ca4366cf5d3bf5a8748aa9a631acdaad3e9f635
Eunhyuk Shin <silvershine157@gmail.com>
2020-02-04 16:57:59 +0900

    training routine

--- adamw/main.py
+++ adamw/main.py
@@ -4,28 +4,62 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def test1():
-	vgg16 = models.vgg11(pretrained=True)
 
-def test2():
+	batch_size = 128
+
 	transform = transforms.Compose([
 		transforms.ToTensor(),
 		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 	])
-
 	trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
 	                                        download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
 	                                          shuffle=True, num_workers=2)
-
 	testset = torchvision.datasets.CIFAR100(root='./data', train=False,
 	                                       download=True, transform=transform)
-	testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+	testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
 	                                         shuffle=False, num_workers=2)
-
 	classes = ('plane', 'car', 'bird', 'cat',
 	           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-	
 
-test2()
+	net = models.vgg11(pretrained=True)
+	#net = models.vgg11(pretrained=False)
+	net.to(device)
+	optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+	for epoch in range(100):
+		loss = train_epoch(net, trainloader, optimizer)
+		print(loss)
+
+
+def test_epoch(net, loader):
+	for batch_idx, batch in enumerate(loader):
+		x, y = batch
+		out = net(x)
+		print(out)
+		break
+
+def train_epoch(net, loader, optimizer):
+	running_n = 0
+	running_loss = 0.0
+	for batch_idx, batch in enumerate(loader):
+		optimizer.zero_grad()
+		x, y = batch
+		x = x.to(device)
+		y = y.to(device)
+		out = net(x)
+		loss = nn.functional.cross_entropy(out, y, reduction='mean')
+		loss.backward()
+		optimizer.step()
+		B = x.size(0)
+		running_n += B
+		running_loss += B*loss.detach().item()
+	loss = running_loss/running_n
+	return loss
+
+test1()
