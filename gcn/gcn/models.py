e4d23f6020ce1da96e8e0ac88be604a20c93721d
yongsu-bloo <yongsubaek7@gmail.com>
2020-03-17 11:53:51 +0900

    GCN architecture

--- gcn/gcn/models.py
+++ gcn/gcn/models.py
@@ -7,8 +7,17 @@ class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
         # TODO 
-        pass
+        self.w1 = nn.Linear(nfeat, nhid)
+        self.w2 = nn.Linear(nhid, nclass)
 
     def forward(self, x, adj):
-        # TODO 
-        pass
+        # TODO
+        '''
+        x: [N, nfeat]
+        adj: [N, N]
+        ---
+        output: [N, nclass]
+        '''
+        h1 = F.relu(self.w1(torch.mm(adj, x))) # [N, nhid]
+        output = F.softmax(self.w2(torch.mm(adj, h1)), dim=1)
+        return output
