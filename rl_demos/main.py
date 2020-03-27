03fdea36af5b3ac5be894c46188e0465edc31121
eunhyoukshin <silvershine157@gmail.com>
2020-03-27 21:19:40 +0900

    MRP first visit MC evaluation

--- rl_demos/main.py
+++ rl_demos/main.py
@@ -32,8 +32,37 @@ class MarkovRewardProcess(object):
 		gamma = 0.5
 		return MarkovRewardProcess(Ns, P, R, gamma)
 
+class evaluate:
+	class mrp:
+		def mc(mrp):
+			# first visit monte carlo
+			episodes = 100
+			horizon = 30 # as an approximation to infinity
+			visit_sum = np.zeros(mrp.Ns, np.int)
+			return_sum = np.zeros(mrp.Ns)
+			for ep in range(episodes):
+				discount = np.zeros(mrp.Ns)
+				visited = np.zeros(mrp.Ns, dtype=np.bool)
+				mrp.initialize()
+				for t in range(horizon):
+					state, reward = mrp.step()
+					if not visited[state]:
+						visit_sum[state] += 1
+						visited[state] = True
+						discount[state] = 1.0
+					return_sum[state] += discount[state]*reward
+					discount = mrp.gamma*discount
+			V = return_sum/visit_sum
+			return V
+
+		def analytic(mrp):
+			pass
+		def iterative(mrp):
+			pass
+
+
 def test1():
-	# MRP evaluation
+	# MRP simulation test
 	Ns = 3
 	mrp = MarkovRewardProcess.create_random(Ns)
 	mrp.initialize()
@@ -41,4 +70,12 @@ def test1():
 		state, reward = mrp.step()
 		print(state, reward)
 
-test1()
+def test2():
+	# compare MRP evaluation methods
+	Ns = 3
+	mrp = MarkovRewardProcess.create_random(Ns)
+	V_mc = evaluate.mrp.mc(mrp)
+	print(V_mc)
+
+test2()
+
