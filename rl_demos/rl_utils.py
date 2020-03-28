569ebfaf7a4677c3e5be3888d78d2fcd656c1414
eunhyoukshin <silvershine157@gmail.com>
2020-03-28 10:43:57 +0900

    split files

--- rl_demos/rl_utils.py
+++ rl_demos/rl_utils.py
@@ -102,49 +102,9 @@ class evaluate:
 def mrp_bellman(V, P, R, gamma):
 	return R + gamma*np.matmul(np.transpose(P), V)
 
-def test1():
-	# MRP simulation
-	Ns = 3
-	mrp = MarkovRewardProcess.create_random(Ns)
-	mrp.initialize()
-	for i in range(5):
-		state, reward = mrp.step()
-		print(state, reward)
-
-def test2():
-	# compare MRP evaluation methods
-	Ns = 5
-	mrp = MarkovRewardProcess.create_random(Ns)
-	print("< MRP evaluation test >")
-	print('MC')
-	V_mc = evaluate.mrp.mc(mrp)
-	print(V_mc)
-	print('analytic')
-	V_analytic = evaluate.mrp.analytic(mrp)
-	print(V_analytic)
-	print('iterative')
-	V_iter = evaluate.mrp.iterative(mrp)
-	print(V_iter)
-
 def random_policy(Ns, Na):
 	'''
 	policy: [Ns, Na]
 	'''
 	policy = np.random.dirichlet(10.0*np.ones(Na), Ns)
 	return policy
-
-def test3():
-	# MDP simulation
-	Ns = 3
-	Na = 5
-	mdp = MarkovDecisionProcess.create_random(Ns, Na)
-	policy = random_policy(Ns, Na)
-	state = mdp.initialize()
-	for _ in range(5):
-		action = np.random.choice(np.arange(Na), p=policy[state, :])
-		print(state, action)
-		state, reward = mdp.step(action)
-		print('reward: {:g}'.format(reward))
-
-test3()
-
---
+++ rl_demos/tests.py
@@ -0,0 +1,43 @@
+import numpy as np
+from rl_utils import *
+
+def test1():
+	# MRP simulation
+	Ns = 3
+	mrp = MarkovRewardProcess.create_random(Ns)
+	mrp.initialize()
+	for i in range(5):
+		state, reward = mrp.step()
+		print(state, reward)
+
+def test2():
+	# compare MRP evaluation methods
+	Ns = 5
+	mrp = MarkovRewardProcess.create_random(Ns)
+	print("< MRP evaluation test >")
+	print('MC')
+	V_mc = evaluate.mrp.mc(mrp)
+	print(V_mc)
+	print('analytic')
+	V_analytic = evaluate.mrp.analytic(mrp)
+	print(V_analytic)
+	print('iterative')
+	V_iter = evaluate.mrp.iterative(mrp)
+	print(V_iter)
+
+
+def test3():
+	# MDP simulation
+	Ns = 3
+	Na = 5
+	mdp = MarkovDecisionProcess.create_random(Ns, Na)
+	policy = random_policy(Ns, Na)
+	state = mdp.initialize()
+	for _ in range(5):
+		action = np.random.choice(np.arange(Na), p=policy[state, :])
+		print(state, action)
+		state, reward = mdp.step(action)
+		print('reward: {:g}'.format(reward))
+
+test3()
+
