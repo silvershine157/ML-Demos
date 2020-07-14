541cc857623bf8b894c9fb7ce6cca58c6aa9ef08
Eunhyuk Shin <silvershine157@gmail.com>
2020-07-14 14:02:02 +0900

    add tacotron text cleaner

--- speech/main.py
+++ speech/main.py
@@ -1,7 +1,21 @@
 import torch
 import torchaudio
+from torchaudio.transforms import MelSpectrogram
 
-# Goal: overfit tacotron 2 to SpeechCommand dataset
+# Goal: overfit tacotron 2 to SpeechCommands dataset
 
-def main():
-	print("Hello!")
+def test1():
+    dataset = torchaudio.datasets.LJSPEECH('./data', download=True)
+    #loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+    a,b,c,d = dataset[0]
+    print(a.shape)
+    print(b)
+    print(c)
+    print(d)
+
+def test2():
+    dataset = torchaudio.datasets.LJSPEECH('./data')
+    wave, fs, _, txt = dataset[0]
+    
+
+test2()
