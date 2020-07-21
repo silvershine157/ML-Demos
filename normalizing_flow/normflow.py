b7835bae3b70ce0f3aa8968b279b677e07e6c55a
Eunhyuk Shin <silvershine157@gmail.com>
2020-07-21 16:50:51 +0900

    MNIST training loop

--- normalizing_flow/normflow.py
+++ normalizing_flow/normflow.py
@@ -1,11 +1,16 @@
 import torch
 import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+import numpy as np
 
 '''
 <convention>
 f: x -> z
 f_inv: z -> x
 '''
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class Flow(nn.Module):
 	def __init__(self):
@@ -43,7 +48,7 @@ class IdentityFlow(Flow):
 		res: [B]
 		'''
 		B = x.size(0)
-		res = torch.zeros(B)
+		res = torch.zeros(B, device=device)
 		return res
 
 class CompositeFlow(Flow):
@@ -70,7 +75,7 @@ class CompositeFlow(Flow):
 		res: [B]
 		'''
 		B = x.size(0)
-		res = torch.zeros(B)
+		res = torch.zeros(B, device=device)
 		for i in range(len(self.subflows)):
 			res += self.subflows[i].log_det_jac(x) # exploit chain rule
 			x = self.subflows[i].f(x)
@@ -101,11 +106,12 @@ class CouplingLayer1D(Flow):
 		net_out = self.net(net_input)
 		log_scale = net_out[:, :self.half_dim]
 		bias = net_out[:, self.half_dim:]
-		z = x.clone()
 		if self.change_first:
-			z[:, :self.half_dim] = torch.exp(log_scale) * z[:, :self.half_dim] + bias
+			modified = torch.exp(log_scale) * x[:, :self.half_dim] + bias
+			z = torch.cat([modified, net_input], dim=1)
 		else:
-			z[:, self.half_dim:] = torch.exp(log_scale) * z[:, self.half_dim:] + bias
+			modified = torch.exp(log_scale) * x[:, self.half_dim:] + bias
+			z = torch.cat([net_input, modified], dim=1)
 		return z
 
 	def f_inv(self, z):
@@ -121,11 +127,12 @@ class CouplingLayer1D(Flow):
 		net_out = self.net(net_input)
 		log_scale = net_out[:, :self.half_dim]
 		bias = net_out[:, self.half_dim:]
-		x = z.clone()
 		if self.change_first:
-			x[:, :self.half_dim] = torch.exp(-log_scale) * (x[:, :self.half_dim] - bias)
+			modified = torch.exp(-log_scale) * (z[:, :self.half_dim] - bias)
+			x = torch.cat([modified, net_input], dim=1)
 		else:
-			x[:, self.half_dim:] = torch.exp(-log_scale) * (x[:, self.half_dim:] - bias)
+			modified = torch.exp(-log_scale) * (z[:, self.half_dim:] - bias)
+			x = torch.cat([net_input, modified], dim=1)
 		return x
 
 	def log_det_jac(self, x):
@@ -143,6 +150,16 @@ class CouplingLayer1D(Flow):
 		res = log_scale.sum(dim=1) # log(det(diag(exp(ls))))=log(prod(exp(ls)))=sum(log(exp(ls)))=sum(ls)
 		return res
 
+def log_pdf_unitnormal(z):
+	'''
+	z: [B, D]
+	---
+	res: [B]
+	'''
+	elewise = -0.5*z**2 - 0.5*np.log(2*np.pi)
+	res = elewise.sum(dim=1)
+	return res
+
 def test1():
 	flows = [IdentityFlow() for _ in range(5)]
 	cflow = CompositeFlow(flows)
@@ -157,10 +174,11 @@ def test1():
 def test2():
 	B = 2
 	full_dim = 4
-	x = torch.randn((B, full_dim))
+	x = torch.randn((B, full_dim), device=device)
 	flow_1 = CouplingLayer1D(full_dim, True)
 	flow_2 = CouplingLayer1D(full_dim, False)
 	flow = CompositeFlow([flow_1, flow_2])
+	flow.to(device)
 	z = flow.f(x)
 	x_r = flow.f_inv(z)
 	ldj = flow.log_det_jac(x)
@@ -169,4 +187,46 @@ def test2():
 	print(x_r)
 	print(ldj)
 
-test2()
+def test3():
+	full_dim = 28*28
+	flow = CompositeFlow([
+		CouplingLayer1D(full_dim, True),
+		CouplingLayer1D(full_dim, False),
+		CouplingLayer1D(full_dim, True),
+		CouplingLayer1D(full_dim, False),
+		CouplingLayer1D(full_dim, True),
+		CouplingLayer1D(full_dim, False),
+	])
+	transform = torchvision.transforms.Compose([
+		torchvision.transforms.ToTensor()
+	])
+	ds = torchvision.datasets.MNIST('./data', download=True, transform=transform)
+	B = 128
+	optimizer = torch.optim.Adam(flow.parameters(), lr=0.00001)
+	loader = DataLoader(ds, batch_size=B)
+	flow.to(device)
+	flow.train()
+	n_epochs = 10
+	for epoch in range(1, n_epochs+1):
+		running_n = 0
+		running_loss = 0.0
+		for batch in loader:
+			x2d, _ = batch # [B, 1, 28, 28]
+			B = x2d.size(0)
+			optimizer.zero_grad()
+			x_flat = x2d.view(B, -1).to(device)
+			log_det_jac = flow.log_det_jac(x_flat)
+			z = flow.f(x_flat) # TODO: factor out redundant computation
+			log_p_z = log_pdf_unitnormal(z)
+			log_p_x = log_p_z + log_det_jac # change of variables
+			loss = -log_p_x.mean() # minimize NLL
+			print(loss)
+			loss.backward()
+			optimizer.step()
+			running_n += B
+			running_loss += B*loss.item()
+		avg_log_p_x = -(running_loss/running_n)
+		print('Epoch {:d} avg log p(x): {:g}'.format(epoch, avg_log_p_x))
+		torch.save(flow.state_dict(), 'data/state_dict/sd_{:d}'.format(epoch))
+
+test3()
