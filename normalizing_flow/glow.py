ee7d565c7cc7914b1b4331eefdf1f4bd2634228c
Eunhyuk Shin <silvershine157@gmail.com>
2020-08-05 16:03:45 +0900

    add CNN coupling

--- normalizing_flow/glow.py
+++ normalizing_flow/glow.py
@@ -1,59 +1,70 @@
 import torch
+import torch.nn as nn
 
 # Glow with additive coupling layer & no LU decomposition
 
-def additive_coupling(h_in):
-	'''
-	h_in: [B, S, S, C]
-	---
-	h_out: [B, S, S, C]
-	'''
-
-	def NN(nn_in):
+class coupling_NN(nn.Module):
+	def __init__(self, C_half):
+		super(coupling_NN, self).__init__()
+		self.layers = nn.Sequential(
+			nn.Conv2d(C_half, 512, 3, padding=1),
+			nn.ReLU(),
+			nn.Conv2d(512, 512, 1),
+			nn.ReLU(),
+			nn.Conv2d(512, C_half, 3, padding=1)
+		)
+	def forward(self, nn_in):
 		'''
-		nn_in: [B, S, S, C//2]
+		nn_in: [B, C_half, S, S]
 		---
-		nn_out: [B, S, S, C//2]
+		nn_out: [B, C_half, S, S]
 		'''
-		nn_out = nn_in # TODO: use 3 layer CNN (parametrized)
+		nn_out = self.layers(nn_in)
 		return nn_out
 
-	_, S, _, C = h_in.shape
+def additive_coupling(h_in):
+	'''
+	h_in: [B, C, S, S]
+	---
+	h_out: [B, C, S, S]
+	'''
+	_, C, S, _ = h_in.shape
 	assert C%2 == 0
-	first_half = h_in[:, :, :, :C//2]
-	nn_out = NN(first_half)
+	net = coupling_NN(C//2)
+	first_half = h_in[:, :C//2, :, :]
+	nn_out = net(first_half)
 	h_out = h_in.clone()
-	h_out[:, :, :, C//2:] += nn_out
+	h_out[:, C//2:, :, :] += nn_out
 	return h_out
 
 def invertible_1x1_conv(h_in):
 	'''
-	h_in: [B, S, S, C]
+	h_in: [B, C, S, S]
 	---
-	h_out: [B, S, S, C]
+	h_out: [B, C, S, S]
 	'''
-	_, _, _, C = h_in.shape
+	_, C, _, _ = h_in.shape
 	W = torch.randn(C, C) # param
-	h_out = torch.einsum('ij,bhwi->bhwj', W, h_in)
+	h_out = torch.einsum('ij,bihw->bjhw', W, h_in)
 	return h_out
 
 def actnorm(h_in):
 	'''
-	h_in: [B, S, S, C]
+	h_in: [B, C, S, S]
 	---
-	h_out: [B, S, S, C]
+	h_out: [B, C, S, S]
 	'''
-	_, _, _, C = h_in.shape
-	scale = torch.randn(1, 1, 1, C) # param
-	bias = torch.randn(1, 1, 1, C) # param
+	_, C, _, _ = h_in.shape
+	scale = torch.randn(1, C, 1, 1) # param
+	bias = torch.randn(1, C, 1, 1) # param
 	h_out = scale * h_in + bias # broadcasting
 	return h_out
 
 def step_of_flow(h_in):
 	'''
-	h_in: [B, S, S, C]
+	h_in: [B, C, S, S]
 	---
-	h_out: [B, S, S, C]
+	h_out: [B, C, S, S]
 	'''
 	h1 = actnorm(h_in)
 	h2 = invertible_1x1_conv(h1)
@@ -61,7 +72,7 @@ def step_of_flow(h_in):
 	return h_out
 
 def test1():
-	h_in = torch.randn(2, 4, 4, 6)
+	h_in = torch.randn(2, 6, 4, 4)
 	h_out = step_of_flow(h_in)
 	print(h_out.shape)
 
