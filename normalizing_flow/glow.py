1406c8f740616d2a7083cc61180e72e108b43fc2
Eunhyuk Shin <silvershine157@gmail.com>
2020-08-05 17:14:54 +0900

    check number of parameters

--- normalizing_flow/glow.py
+++ normalizing_flow/glow.py
@@ -96,12 +96,21 @@ class Glow(nn.Module):
 		pass
 
 
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
+
 def test1():
 	C = 1
-	K = 10
+	K = 4
 	L = 3
 	glow = Glow(K, L, C)
-	print(len(glow.parameters()))
+	print(get_n_params(glow))
 	pass
 
 test1()
